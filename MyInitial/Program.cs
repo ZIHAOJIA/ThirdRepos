331122db971e25ca633470c6e16d122166ace1f5
ZIHAOJIA <zihao.jia@student.tafesa.edu.au>
2024-03-08 14:19:41 +1030

    11

--- MyInitial/Program.cs
+++ MyInitial/Program.cs
@@ -14,6 +14,7 @@ namespace MyNameSpace
         [STAThread]
         static void Main()
         {
+            //11
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TicketsForm());
